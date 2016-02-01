e8e5474319be0a2bfbd7d492695459a10cb9d125
lijinyao <lijinyao@oneniceapp.com>
2016-02-01 17:43:17 +0800

    ndk

--- image_edit_library/src/main/jni/photo_processing.c
+++ image_edit_library/src/main/jni/photo_processing.c
@@ -17,7 +17,8 @@
 #include <jni.h>
 #include <stdlib.h>
 #include <android/log.h>
-
+#include <bitmap.h>
+#include <mem_utils.h>
 #define  LOG_TAG    "PREVIEW_CACHE_IMAGE_PROCESSING"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
